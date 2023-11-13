c46cf322acce826965c5330ac9d55eaaee08fae4
bakhanh113ct <84719735+bakhanh113ct@users.noreply.github.com>
2023-11-13 22:03:43 +0700

    first commit

---
+++ src/email.py
@@ -0,0 +1,68 @@
+from src.constants.http_status_codes import HTTP_200_OK, HTTP_201_CREATED, HTTP_204_NO_CONTENT, HTTP_400_BAD_REQUEST, HTTP_404_NOT_FOUND, HTTP_409_CONFLICT
+from flask import Blueprint, request
+from flask.json import jsonify
+import validators
+from flask_jwt_extended import get_jwt_identity, jwt_required
+from src.database import Email, db, User
+from flasgger import swag_from
+
+emails = Blueprint("emails", __name__, url_prefix="/api/v1/emails")
+
+
+@emails.route('/')
+@jwt_required()
+def getAllEmail():
+    current_user = get_jwt_identity()
+    page = request.args.get('page', 1, type=int)
+    per_page = request.args.get('per_page', 5, type=int)
+
+    emails = Email.query.filter_by(
+        user_id=current_user).paginate(page=page, per_page=per_page)
+
+    data = []
+
+    for email in emails.items:
+        data.append({
+            "id": email.id,
+            "title": email.title,
+            "body": email.body,
+            "user_id": email.user_id,
+            "receiver_id": email.receiver_id,
+            "is_spam": email.is_spam
+        })
+
+    meta = {
+        "page": emails.page,
+        'pages': emails.pages,
+        'total_count': emails.total,
+        'prev_page': emails.prev_num,
+        'next_page': emails.next_num,
+        'has_next': emails.has_next,
+        'has_prev': emails.has_prev,
+    }
+
+    return jsonify({"data": data, "meta": meta})
+
+
+@emails.route('/', methods=["POST"])
+@jwt_required()
+def create_email():
+    current_user = get_jwt_identity()
+
+    title = request.get_json().get('title', '')
+    body = request.get_json().get('body', '')
+    receiver_email = request.get_json().get('receiver_email', '')
+
+    receiver_user = User.query.filter_by(email=receiver_email).first()
+
+    if (receiver_user is None):
+        return jsonify({
+            'error': 'Email is not exist'
+        }), HTTP_400_BAD_REQUEST
+
+    email = Email(title=title, body=body, user_id=current_user,
+                  receiver_id=receiver_user.id)
+    db.session.add(email)
+    db.session.commit()
+
+    return jsonify(email)
